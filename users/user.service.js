db10ef84cfa125af76875556b644b3007ecaeae1
eugene <evgen.zaharevich@gmail.com>
2018-08-31 18:12:35 +0300

    add

--- users/user.service.js
+++ users/user.service.js
@@ -2,15 +2,16 @@
 const jwt = require('jsonwebtoken');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [{ id: 1, email: 'test@test', password: 'test', firstName: 'Test', lastName: 'User' },
+    { id: 2, username: 'test2', password: 'test2', firstName: 'Test2', lastName: 'User2'} ];
 
 module.exports = {
     authenticate,
     getAll
 };
 
-async function authenticate({ username, password }) {
-    const user = users.find(u => u.username === username && u.password === password);
+async function authenticate({ email, password }) {
+    const user = users.find(u => u.email === email && u.password === password);
     if (user) {
         const token = jwt.sign({ sub: user.id }, config.secret);
         const { password, ...userWithoutPassword } = user;
